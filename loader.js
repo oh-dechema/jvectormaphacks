8f846c2f51786efd32625422e78e21baf8c058a9
oh-dechema <36410376+oh-dechema@users.noreply.github.com>
2018-03-01 16:42:43 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -52,6 +52,7 @@ var plants = [
 	{name: 'Julius-Maximilians-Universität Würzburg - Fakultät für Chemie und Pharmazie - Institut für Organische Chemie', coords: [49.7804649,9.9668576], partner: ['CarbonCat']},
   ];
 var partnerMap;
+var setCoords = [];
 $(document).ready(function() {
     // jQuery code goes here
 	partnerMap = $('#map').vectorMap({
@@ -76,7 +77,31 @@ $(document).ready(function() {
 			  },
 		},
 		map: 'de_merc',
-		markers: plants.map(function(h){ return {name: h.name, latLng: h.coords} }),
+		markers: plants.map(function(h){
+			const lat1 = h.coords[0],
+			      lon1 = h.coords[1],
+			      R = 6371e3, // metres
+			      φ1 = lat1.toRadians();
+			for(let i=0;i<setCoords.length;i++){
+				const lat2 = setCoords[i][0],
+				      lon2 = setCoords[i][1],
+				      φ2 = lat2.toRadians(),
+				      Δφ = (lat2-lat1).toRadians(),
+				      Δλ = (lon2-lon1).toRadians();
+				var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+					Math.cos(φ1) * Math.cos(φ2) *
+					Math.sin(Δλ/2) * Math.sin(Δλ/2);
+				var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+				var d = R * c;
+				console.log(d);
+				if(d<2000){
+					h.coords[0] = h.coords[0] + 0.1;
+					i = 0;
+				}
+			}
+			setCoords.push(h.coords);
+			return {name: h.name, latLng: h.coords}
+		}),
 		series: {
 		  markers: [
 			{
